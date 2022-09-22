7751590546666765efe4c68ee8f392de5892d15f
TranThiKimLinhB1910402 <kimlinhtr@gmail.com>
2022-09-22 10:09:05 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const contactsRouter = require("./app/routes/contact.route");
 const app = express();
@@ -12,5 +13,15 @@ app.use("/api/contacts", contactsRouter);
 app.get("/",(req, res) => {
     res.json({message: "Welcome to contact book application"});
 });
+
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+})
+
+app.use((err, req, res, next) =>{
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal server Error",
+    });
+})
 
 module.exports = app;
